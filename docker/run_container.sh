b25d6f6ee74c8448218efa3bcf7c08b7f34c8189
RexBarker <rexbarker2017@gmail.com>
2020-07-04 23:54:05 +0200

    changed run architecture to two docker contianers

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -4,4 +4,4 @@ docker run --gpus all -it  --shm-size=8gb --env="DISPLAY" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 	--volume=$HOME/.torch/fvcore_cache:/tmp:rw  \
 	-p 8889:8889/tcp -p 48171:22 \
-	--name=detect_inpaint detect_inpaint:v0
+	--name=detect_inpaintv1 detect_inpaint:v1
